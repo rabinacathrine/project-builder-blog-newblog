5c84838f22209945f122c66f3ec560b00855cea6
Rabina <32222576+rabinacathrine@users.noreply.github.com>
2020-03-26 16:51:03 +0530

    prograd

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -10,12 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Blog;
 
+import model.User;
 
 
 @WebServlet(urlPatterns= {"/blog"})
 public class BlogController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Object user;
        
 
     public BlogController() {
@@ -32,19 +35,31 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
-		
+		String blogDetails = request.getParameter("selectedAnswers");
+		String arr[]=blogDetails.split(",");
+		String title=arr[0];
+		String description=arr[1];
+		LocalDate postedOn= LocalDate.now(); 
+		Blog blog=new Blog(title,description,postedOn);
+		blog.setTitle(title);
+		//blog.setDescription(description);
+		blog.setDescription(description);
+		blog.setPostedon(postedOn);
+		System.out.println("Blog Title: "+blog.getTitle());
+		System.out.println("Blog Description: "+blog.getDescription());
+		System.out.println("Posted on: "+blog.getPostedon());
 		
 		
 
 		
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
-			request.setAttribute("user",user);
+			//request.setAttribute("User",User);
+			request.setAttribute("user", user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+	
 	}
 
 }
--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,36 @@
 package model;
 // Type your code
+import java.time.*;
+public class Blog{
+	String title;
+	String description;
+	LocalDate postedOn;
+
+	public String getTitle(){
+		return title;
+	}
+public void setTitle(String title){
+	this.title=title;
+}
+
+public String getDescription(){
+	return description;
+}
+public void setDescription(String description){
+this.description=description;
+}
+
+public LocalDate getPostedon(){
+	return postedOn;
+}
+public void setPostedon(LocalDate postedOn){
+this.postedOn=postedOn;
+}
+
+//public Blog(String title,int description,LocalDate postedOn){
+//	
+//}
+public Blog(String title, String description, LocalDate date) {
+	// TODO Auto-generated constructor stub
+}
+}
